05dbff208d2b0f28875f7e51c6508da8defc94e8
Antoine AUBÉ <aaube@axonic.fr>
2018-09-19 16:50:41 +0200

    Switch DAO to Mongo for bus

--- avek-bus/src/main/java/fr/axonic/avek/bus/configuration/JustificationBusBinder.java
+++ avek-bus/src/main/java/fr/axonic/avek/bus/configuration/JustificationBusBinder.java
@@ -2,14 +2,14 @@ package fr.axonic.avek.bus.configuration;
 
 import fr.axonic.avek.bus.services.StepBuilder;
 import fr.axonic.avek.dao.JustificationSystemsDAO;
-import fr.axonic.avek.dao.implementations.SimpleJustificationSystemsDAO;
+import fr.axonic.avek.dao.implementations.MongoJustificationSystemsDAO;
 import org.glassfish.jersey.internal.inject.AbstractBinder;
 
 public class JustificationBusBinder extends AbstractBinder {
 
     @Override
     protected void configure() {
-        bind(SimpleJustificationSystemsDAO.class).to(JustificationSystemsDAO.class);
+        bind(MongoJustificationSystemsDAO.class).to(JustificationSystemsDAO.class);
         bind(StepBuilder.class).to(StepBuilder.class);
     }
 }
