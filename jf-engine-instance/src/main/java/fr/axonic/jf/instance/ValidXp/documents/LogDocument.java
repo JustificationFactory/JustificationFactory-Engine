bc46188f5edde00fa9b4c33eccd490ddc24c90b7
Habibou Sissoko <habibou.s@outlook.fr>
2018-11-28 16:11:43 +0100

    Add LogDocument to handle Log evidence

---
+++ jf-engine-instance/src/main/java/fr/axonic/jf/instance/ValidXp/documents/LogDocument.java
@@ -0,0 +1,57 @@
+package fr.axonic.jf.instance.ValidXp.documents;
+
+import fr.axonic.jf.engine.support.evidence.Document;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.util.Objects;
+
+@XmlRootElement
+public class LogDocument extends Document{
+
+    private String jobId;
+    private int exitCode;
+
+    public LogDocument(String url, String jobId, int exitCode) {
+        super(url);
+        this.jobId = jobId;
+        this.exitCode = exitCode;
+    }
+
+    public LogDocument() {
+    }
+
+
+    @XmlElement
+    public String getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(String jobId) {
+        this.jobId = jobId;
+    }
+
+    @XmlElement
+    public int getExitCode() {
+        return exitCode;
+    }
+
+    public void setExitCode(int exitCode) {
+        this.exitCode = exitCode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        LogDocument that = (LogDocument) o;
+        return exitCode == that.exitCode &&
+                Objects.equals(jobId, that.jobId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), jobId, exitCode);
+    }
+}
--- jf-engine-instance/src/main/java/fr/axonic/jf/instance/ValidXp/evidence/LogEvidence.java
+++ jf-engine-instance/src/main/java/fr/axonic/jf/instance/ValidXp/evidence/LogEvidence.java
@@ -1,15 +1,16 @@
 package fr.axonic.jf.instance.ValidXp.evidence;
 
-import fr.axonic.jf.engine.support.evidence.Document;
+//import fr.axonic.jf.engine.support.evidence.Document;
 import fr.axonic.jf.engine.support.evidence.Evidence;
+import fr.axonic.jf.instance.ValidXp.documents.LogDocument;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class LogEvidence extends Evidence<Document> {
+public class LogEvidence extends Evidence<LogDocument> {
     public static final String EVIDENCE_NAME = "LOG_E";
 
-    public LogEvidence(Document element) {
+    public LogEvidence(LogDocument element) {
         super(EVIDENCE_NAME, element);
     }
 
