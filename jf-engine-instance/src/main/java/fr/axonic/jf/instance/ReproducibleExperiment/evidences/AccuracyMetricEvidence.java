ab537ee016a2349f578e759a92ee85cb68d240d2
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-12-07 11:25:21 +0100

    Fix error ; extends evidences instead of conclusions

--- jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/evidences/AccuracyMetricEvidence.java
+++ jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/evidences/AccuracyMetricEvidence.java
@@ -2,15 +2,16 @@ package fr.axonic.jf.instance.ReproducibleExperiment.evidences;
 
 import fr.axonic.jf.engine.support.conclusion.Conclusion;
 import fr.axonic.jf.engine.support.evidence.Document;
+import fr.axonic.jf.engine.support.evidence.Evidence;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class AccuracyMetricEvidence extends Conclusion<Document> {
+public class AccuracyMetricEvidence extends Evidence<Document> {
     public static final String EVIDENCE_NAME = "ACCURACY_METRIC_EVIDENCE";
 
-    public AccuracyMetricEvidence(String name, Document element) {
-        super(name, element);
+    public AccuracyMetricEvidence(Document element) {
+        super(EVIDENCE_NAME, element);
     }
 
     public AccuracyMetricEvidence() {
--- jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/evidences/TotalTimeMetricEvidence.java
+++ jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/evidences/TotalTimeMetricEvidence.java
@@ -1,16 +1,16 @@
 package fr.axonic.jf.instance.ReproducibleExperiment.evidences;
 
-import fr.axonic.jf.engine.support.conclusion.Conclusion;
 import fr.axonic.jf.engine.support.evidence.Document;
+import fr.axonic.jf.engine.support.evidence.Evidence;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
-public class TotalTimeMetricEvidence extends Conclusion<Document> {
+public class TotalTimeMetricEvidence extends Evidence<Document> {
     public static final String EVIDENCE_NAME = "TOTAL_TIME_METRIC_EVIDENCE";
 
-    public TotalTimeMetricEvidence(String name, Document element) {
-        super(name, element);
+    public TotalTimeMetricEvidence(Document element) {
+        super(EVIDENCE_NAME, element);
     }
 
     public TotalTimeMetricEvidence() {
