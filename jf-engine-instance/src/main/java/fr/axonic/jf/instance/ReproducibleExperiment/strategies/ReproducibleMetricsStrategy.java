fd6d9a92387b80c77a55b678b23d264495f6358b
Aurélien Spinelli <aurelien.spinelli@hotmail.fr>
2018-12-12 15:53:54 +0100

    Implement ReproducibleMetricStrategy ; check jobId and reproducible

--- jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/strategies/ReproducibleMetricsStrategy.java
+++ jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/strategies/ReproducibleMetricsStrategy.java
@@ -3,7 +3,9 @@ package fr.axonic.jf.instance.ReproducibleExperiment.strategies;
 import fr.axonic.jf.engine.support.Support;
 import fr.axonic.jf.engine.support.conclusion.Conclusion;
 import fr.axonic.jf.engine.support.evidence.Document;
+import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.AccuracyMetricConclusion;
 import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.ReproducibleMetricsConclusion;
+import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.TotalTimeMetricConclusion;
 import fr.axonic.jf.instance.ReproducibleExperiment.documents.ReproducibleDocument;
 import fr.axonic.jf.instance.ValidXp.documents.XpDocument;
 
@@ -19,8 +21,26 @@ public class ReproducibleMetricsStrategy extends ReproducibleExperimentStrategy 
 
     @Override
     public Conclusion createConclusion(List<Support> supportList) {
-        ReproducibleDocument doc = new ReproducibleDocument("testReproducible",true);
-        ReproducibleMetricsConclusion conclusion = new ReproducibleMetricsConclusion("REPRODUCIBLE_METRICS_CONCLUSION",doc);
+        ReproducibleMetricsConclusion conclusion = new ReproducibleMetricsConclusion("REPRODUCIBLE_METRICS_CONCLUSION", null);
+        for(int i = 0; i < supportList.size(); i++){
+            Support s = supportList.get(i);
+            if(s instanceof AccuracyMetricConclusion){
+                for (int y = 0; y < supportList.size(); y++){
+                    Support tmp = supportList.get(y);
+                    if(tmp instanceof TotalTimeMetricConclusion){
+                        ReproducibleDocument totalTimeMetricDocument = (ReproducibleDocument) tmp.getElement();
+                        ReproducibleDocument accuracyMetricDocument = (ReproducibleDocument) s.getElement();
+                        String ttmc = totalTimeMetricDocument.getJobId();
+                        String amc = accuracyMetricDocument.getJobId();
+                        if(ttmc.equals(amc)){
+                            boolean reproducible = totalTimeMetricDocument.isReproducible() && accuracyMetricDocument.isReproducible();
+                            ReproducibleDocument doc = new ReproducibleDocument(totalTimeMetricDocument.getJobId(),reproducible);
+                            conclusion = new ReproducibleMetricsConclusion("REPRODUCIBLE_METRICS_CONCLUSION", doc);
+                        }
+                    }
+                }
+            }
+        }
         return conclusion;
     }
 }
