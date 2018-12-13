0b7446230e22dfba62a3377044e0d78908e2d22e
Goncalves.Fernandes.Junior <fernandesgjunior@gmail.com>
2018-12-13 15:53:06 +0100

    modification of the top level strategy for the diagram of reproducibility

--- jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/strategies/ReproducibleExperimentFinalStrategy.java
+++ jf-engine-instance/src/main/java/fr/axonic/jf/instance/ReproducibleExperiment/strategies/ReproducibleExperimentFinalStrategy.java
@@ -3,10 +3,7 @@ package fr.axonic.jf.instance.ReproducibleExperiment.strategies;
 import fr.axonic.jf.engine.support.Support;
 import fr.axonic.jf.engine.support.conclusion.Conclusion;
 import fr.axonic.jf.engine.support.evidence.Document;
-import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.AccuracyMetricConclusion;
-import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.ReproducibleExperimentConclusion;
-import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.ReproducibleMetricsConclusion;
-import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.TotalTimeMetricConclusion;
+import fr.axonic.jf.instance.ReproducibleExperiment.conclusion.*;
 import fr.axonic.jf.instance.ReproducibleExperiment.documents.ReproducibleDocument;
 import fr.axonic.jf.instance.ValidXp.conclusion.ValidXpConclusion;
 import fr.axonic.jf.instance.ValidXp.documents.XpDocument;
@@ -26,24 +23,36 @@ public class ReproducibleExperimentFinalStrategy extends ReproducibleExperimentS
         ReproducibleDocument doc = null;
         for (int i = 0; i < supportList.size(); i++){
             Support s = supportList.get(i);
-            if(s instanceof ReproducibleMetricsConclusion){
-                for (int j = 0; j < supportList.size(); j++){
-                    Support  s2 = supportList.get(j);
-                    if(s2 instanceof ValidXpConclusion){
-                        ReproducibleDocument ReproducibleConclusionDoc = (ReproducibleDocument)s.getElement();
+            if(s instanceof PositiveReproducibleMetricsConclusion) {
+                for (int j = 0; j < supportList.size(); j++) {
+                    Support s2 = supportList.get(j);
+                    if (s2 instanceof ValidXpConclusion) {
+                        ReproducibleDocument ReproducibleConclusionDoc = (ReproducibleDocument) s.getElement();
                         XpDocument ValidXPConclusionDoc = (XpDocument) s2.getElement();
                         String ReproducibleConclusionJobId = ReproducibleConclusionDoc.getJobId();
                         String ValidXPConclusionJobId = ValidXPConclusionDoc.getJobId();
-                        if (ReproducibleConclusionJobId.equals(ValidXPConclusionJobId)){
+                        if (ReproducibleConclusionJobId.equals(ValidXPConclusionJobId)) {
                             boolean reproducibleXP = ReproducibleConclusionDoc.isReproducible() && ValidXPConclusionDoc.getValid();
-                            doc = new ReproducibleDocument(ReproducibleConclusionJobId,reproducibleXP);
-                        }
+                            doc = new ReproducibleDocument(ReproducibleConclusionJobId, reproducibleXP);
+                            if (reproducibleXP) {
+                                ReproducibleExperimentConclusion conclusion = new ReproducibleExperimentConclusion("REPRODUCIBLE_EXPERIMENT_CONCLUSION", doc);
+                                return conclusion;
+                            } else {
+                                ReproducibleExperimentConclusion conclusion = new ReproducibleExperimentConclusion("REPRODUCIBLE_EXPERIMENT_CONCLUSION", doc);
+                                return conclusion;
+                            }
 
+                        }
                     }
                 }
+            }
+            if (s instanceof NegativeReproducibleMetricsConclusion){
+                ReproducibleExperimentConclusion conclusion = new ReproducibleExperimentConclusion("REPRODUCIBLE_EXPERIMENT_CONCLUSION", doc);
+                return conclusion;
             }
         }
         ReproducibleExperimentConclusion conclusion = new ReproducibleExperimentConclusion("REPRODUCIBLE_EXPERIMENT_CONCLUSION",doc);
         return conclusion;
     }
+
 }
